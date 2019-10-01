737d7c19860d40b88cd06e5e8bd27d07ba8fabe6
Andy <andy@Andys-MacBook-Pro.local>
2019-10-01 16:13:13 +0200

    Changed Daro to diario

--- diario-oficial/e2e/src/app.e2e-spec.ts
+++ diario-oficial/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to diaro-oficial!');
+    expect(page.getTitleText()).toEqual('Welcome to diario-oficial!');
   });
 
   afterEach(async () => {
--- diario-oficial/src/app/app.component.spec.ts
+++ diario-oficial/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'diaro-oficial'`, () => {
+  it(`should have as title 'diario-oficial'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('diaro-oficial');
+    expect(app.title).toEqual('diario-oficial');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to diaro-oficial!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to diario-oficial!');
   });
 });
